ee4e939f9ce001836eb76a237111f899170d42d4
Chris Mather <chrismather21@outlook.com>
2022-11-26 18:30:04 +0000

    Changed TV Series page

--- src/pages/tv-series.tsx
+++ src/pages/tv-series.tsx
@@ -3,26 +3,33 @@ import { useShow } from '../hooks/use-show';
 import Carousel from '../components/carousel/carousel';
 import ShowCard from '../components/show-card/show-card';
 import { GridLayout, PagesContainer } from './pages.style';
+import { PagesBanner, PagesHeader } from './pages.style';
 import { CarouselHeader } from '../components/carousel/carousel.style';
 
 function TvSeries() {
   const { popularTvShows, upcomingTv } = useShow();
 
   return (
-    <PagesContainer>
-      <Carousel
-        title={'On TV Now'}
-        data={upcomingTv.map((show) => {
-          return <ShowCard key={show.id} show={show} />;
-        })}
-      />
-      <CarouselHeader>All TV Series</CarouselHeader>
-      <GridLayout>
-        {popularTvShows.map((show) => {
-          return <ShowCard key={show.id} show={show} />;
-        })}
-      </GridLayout>
-    </PagesContainer>
+    <>
+      <PagesBanner>
+        <PagesHeader>Explore TV Series...</PagesHeader>
+      </PagesBanner>
+      <PagesContainer>
+        <Carousel
+          title={'On TV Now'}
+          data={upcomingTv.map((show) => {
+            console.log(typeof show.id, 'Show ID');
+            return <ShowCard key={show.id} show={show} />;
+          })}
+        />
+        <CarouselHeader>All TV Series</CarouselHeader>
+        <GridLayout>
+          {popularTvShows.map((show) => {
+            return <ShowCard key={show.id} show={show} />;
+          })}
+        </GridLayout>
+      </PagesContainer>
+    </>
   );
 }
 
